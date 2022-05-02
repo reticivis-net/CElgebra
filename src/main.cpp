7d5902a2c6943ee1351ef5f21ee5330a54bd98e3
melody <35177464+HexCodeFFF@users.noreply.github.com>
2022-05-01 19:21:57 -0500

    pretty good drawing routine

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,60 @@
 #include <tice.h>
-#include <fontlibc.h>
-#include <graphx.h>
-#include <keypadc.h>
+//#include <fontlibc.h>
+//#include <graphx.h>
+//#include <keypadc.h>
 #include <debug.h>
-#include "melgebra/melgebra.hpp"
+#include <cstring>
+#include <cstdlib>
+//#include "melgebra/melgebra.hpp"
+
+uint8_t rowmax = 14;
+uint8_t colmax = 25;
+struct curpos {
+    uint8_t row = 0; // 0-14
+    uint8_t col = 0; // 0-25
+};
+curpos pos = {0, 0};
+
+void init_terminal() {
+    boot_ClearVRAM();
+    os_FontSelect(reinterpret_cast<font_t *>(1));
+    pos = {0, 0};
+}
+
+void draw_at_pos(const char *string, curpos p) {
+    os_FontDrawText(string, 2 + 12 * p.col, 2 + 16 * p.row);
+}
+
+void draw_text(const char *string) {
+    curpos origpos = pos;
+    size_t len = strlen(string) + 1;
+    // assign area to do stuff with extra space for auto-newlines i think
+    char *line = static_cast<char *>(malloc(colmax + 2));
+    // continually add newlines where needed
+    size_t orig_index, new_index;
+    for (orig_index = 0, new_index = 0; orig_index < len; ++orig_index, ++new_index) {
+        if (string[orig_index] == '\n' || string[orig_index] == '\0' || pos.col > colmax) { // newline
+            line[new_index] = '\0'; // terminate
+            draw_at_pos(line, origpos); // draw
+            if (string[orig_index] == '\0') {
+                return;
+            }
+            // reset
+            pos.col = 0;
+            pos.row++;
+            origpos = pos;
+            new_index = 0;
+        }
+        line[new_index] = string[orig_index];
+        pos.col++;
+    }
+}
 
 int main() {
-    // Start the graphics routines
-    gfx_Begin();
-    kb_EnableOnLatch();
-    fontlib_font_t *oslfont;
-    fontlib_font_t *ossfont;
-    oslfont = fontlib_GetFontByIndex("OSLFONT", 0);
-    if (!oslfont) {
-        gfx_PrintStringXY(
-                "font not found or invalid\n go to https://www.ticalc.org/archives/files/fileinfo/476/47627.html", 0,
-                0);
-        return 1;
-    }
-    ossfont = fontlib_GetFontByIndex("OSsfont", 0);
-    if (!ossfont) {
-        gfx_PrintStringXY(
-                "font not found or invalid\n go to https://www.ticalc.org/archives/files/fileinfo/476/47627.html", 0,
-                0);
-        return 1;
-    }
-    /* Use font for whatever */
-    fontlib_SetFont(oslfont, static_cast<fontlib_load_options_t>(0));
-
-
-    fontlib_SetWindow(4, 4, LCD_WIDTH - 8, 13 * 18);
-    fontlib_HomeUp();
-    fontlib_SetLineSpacing(2, 2);
-    fontlib_SetNewlineOptions(FONTLIB_ENABLE_AUTO_WRAP | FONTLIB_AUTO_SCROLL | FONTLIB_PRECLEAR_NEWLINE);
-    do {
-        kb_Scan();
-        if (kb_IsDown(kb_KeyEnter)) {
-            fontlib_DrawInt(rand(), 8);
-            fontlib_Newline();
-//            msleep(100);
-        }
-//        gfx_SwapDraw();
-    } while (!kb_On);
-
-    // End graphics drawing
-    gfx_End();
-    kb_ClearOnLatch();
-
+    init_terminal();
+    draw_text("123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    draw_text("123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    draw_text("123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    sleep(5);
     return 0;
 }
