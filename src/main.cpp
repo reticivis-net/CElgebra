9e9ad1bfbb8e04f3634e4943e893b15ffe9a654a
melody <35177464+HexCodeFFF@users.noreply.github.com>
2022-05-01 20:54:47 -0500

    iss aiight

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,110 @@
 #include <tice.h>
 //#include <fontlibc.h>
-//#include <graphx.h>
+#include <graphx.h>
 //#include <keypadc.h>
 #include <debug.h>
 #include <cstring>
 #include <cstdlib>
 //#include "melgebra/melgebra.hpp"
 
-uint8_t rowmax = 14;
-uint8_t colmax = 25;
+uint8_t lrowmax = 14;
+uint8_t lcolmax = 25;
+uint8_t srowmax = 19;
+uint8_t scolmax = 40;
 struct curpos {
-    uint8_t row = 0; // 0-14
-    uint8_t col = 0; // 0-25
+    uint8_t row = 0;
+    uint8_t col = 0;
 };
-curpos pos = {0, 0};
+curpos largepos = {0, 0};
+curpos smallpos = {0, 0};
+
+enum stateenum {
+    none,
+    second,
+    alphaupper,
+    alphalower
+};
+
+stateenum calcstate = stateenum::none;
+bool alock = false;
 
 void init_terminal() {
     boot_ClearVRAM();
     os_FontSelect(reinterpret_cast<font_t *>(1));
-    pos = {0, 0};
+    largepos = {0, 0};
+    smallpos = {0, 0};
 }
 
-void draw_at_pos(const char *string, curpos p) {
-    os_FontDrawText(string, 2 + 12 * p.col, 2 + 16 * p.row);
+void draw_at_pos(const char *string, curpos p, bool large = true) {
+    os_FontDrawText(string, large ? 2 + 12 * p.col : 10 * p.col + 1, large ? 2 + 16 * p.row : 12 * p.row);
 }
 
-void draw_text(const char *string) {
-    curpos origpos = pos;
+void draw_text(const char *string, bool large = true) {
+    // draw text using OS routines at the cursor position
+    // WARNING: this routine assumes text is monospace but small text isn't soooooooooooooo
+    curpos *posp;
+    uint8_t colmax;
+    if (large) {
+        os_FontSelect(reinterpret_cast<font_t *>(1));
+        posp = &largepos;
+        colmax = lcolmax;
+    } else {
+        os_FontSelect(reinterpret_cast<font_t *>(0));
+        posp = &smallpos;
+        colmax = scolmax;
+    }
+    curpos* pos = posp;
+    curpos origpos = *pos;
     size_t len = strlen(string) + 1;
     // assign area to do stuff with extra space for auto-newlines i think
     char *line = static_cast<char *>(malloc(colmax + 2));
     // continually add newlines where needed
     size_t orig_index, new_index;
     for (orig_index = 0, new_index = 0; orig_index < len; ++orig_index, ++new_index) {
-        if (string[orig_index] == '\n' || string[orig_index] == '\0' || pos.col > colmax) { // newline
+        if (string[orig_index] == '\n' || string[orig_index] == '\0' || (*pos).col > colmax) { // newline
             line[new_index] = '\0'; // terminate
-            draw_at_pos(line, origpos); // draw
-            if (string[orig_index] == '\0') {
+            draw_at_pos(line, origpos, large); // draw
+            if (string[orig_index] == '\n') { // don't draw nelines
+                ++orig_index;
+                if (string[orig_index] == '\0') { // terminating with \n is a special case
+                    (*pos).col = 0;
+                    (*pos).row++;
+                    return;
+                }
+            }
+            if (string[orig_index] == '\0') { // return if terminated
                 return;
             }
             // reset
-            pos.col = 0;
-            pos.row++;
-            origpos = pos;
+            (*pos).col = 0;
+            (*pos).row++;
+            origpos = (*pos);
             new_index = 0;
         }
         line[new_index] = string[orig_index];
-        pos.col++;
+        (*pos).col++;
+    }
+}
+
+void init_status_bar() {
+    // set top to grey
+    auto *p = (uint16_t *) gfx_vram;
+    for (size_t i = 0; i < LCD_WIDTH * 2 * 17 / sizeof(*p); i++) {
+        p[i] = 0x52AA;
     }
 }
 
 int main() {
     init_terminal();
-    draw_text("123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
-    draw_text("123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
-    draw_text("123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
-    sleep(5);
+    init_status_bar();
+    os_SetDrawFGColor(0xffff);
+    os_SetDrawBGColor(0x52AA);
+    draw_text("AMONGUS AARGH   A", false);
+    os_SetDrawBGColor(0xffff);
+    os_SetDrawFGColor(0x0);
+    largepos = {1, 0};
+    draw_text("some kind of generic text with spaces hehehaha grrr");
+    draw_text("1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+//    sleep(5);
     return 0;
 }
